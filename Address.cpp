cd007800b9b43edf471bdf54e465d96c3231d852
Alejandro Rayas <alexrayas@rocketmail.com>
2019-10-11 23:24:51 -0700

    Prep

---
+++ Address.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Alejandro on 10/11/2019.
+//
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Address.h"
---
+++ gender.h
@@ -0,0 +1,14 @@
+//
+// Created by Alejandro on 10/11/2019.
+//
+
+#ifndef SIM_HOSPITAL_REGISTRATION_AND_DATABASE_GENDER_H
+#define SIM_HOSPITAL_REGISTRATION_AND_DATABASE_GENDER_H
+#include <string>
+#include <vector>
+using namespace std;
+
+enum gender {
+        Male, Female, Other};
+
+#endif //SIM_HOSPITAL_REGISTRATION_AND_DATABASE_GENDER_H
